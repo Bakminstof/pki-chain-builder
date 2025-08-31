971e967600aedea3366e0fcd7e5f0a467a7dd5dc
Bakminstof <bakminstof@yandex.ru>
2025-08-31 14:00:06 +0300

    chore: add get-repo.cmd

--- README.md
+++ README.md
@@ -5,8 +5,18 @@
 curl -sSL https://github.com/Bakminstof/pki-chain-builder/raw/refs/heads/release/scripts/linux/get-repo.sh | bash
 ```
 
+#### Windows
+```bash
+curl -sSL https://github.com/Bakminstof/pki-chain-builder/raw/refs/heads/release/scripts/win/get-repo.cmd | cmd -c
+```
+
 ### Use Linux
 ```bash
 cd pki-chain-builder
 ./scripts/linux/run-pki-builder.sh
+```
+### Use Windows
+```bash
+cd pki-chain-builder/scripts/win
+run-pki-builder.cmd
 ```
---
+++ scripts/win/get-repo.cmd
@@ -0,0 +1,28 @@
+@ECHO off
+
+:: Windows 10
+
+@SET "ENCODING=65001"
+@SET "CURRENT_DIR=%~dp0"
+@SET "SCRIPT_NAME=Get repository"
+
+:: Pre start
+@CHCP %ENCODING% > nul
+
+@TITLE %SCRIPT_NAME%
+:: Start
+::==============================================================================================================
+@SET "REPO_URL=https://github.com/Bakminstof/pki-chain-builder"
+@SET "REPO_NAME=pki-chain-builder"
+@SET "BRANCH=release"
+
+
+git clone --depth=1 --single-branch --branch "%BRANCH%" --no-checkout "%REPO_URL%" "%REPO_NAME%"
+
+@CD "%REPO_NAME%"
+
+git sparse-checkout set --no-cone "/.gitattributes" "/.gitignore" "!/env" "/env/python-3.12.10-embed-amd64-win" "/scripts" "/src" "/LICENSE" "/pyproject.toml"
+
+git checkout "$BRANCH"
+git reset --hard "origin/$BRANCH"
+git pull -X theirs "origin" "$BRANCH"

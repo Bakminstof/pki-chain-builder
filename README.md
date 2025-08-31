9dfce4c46f3ab008160533dc678eee0e9688571c
Bakminstof <bakminstof@yandex.ru>
2025-08-31 12:08:43 +0300

    chore:  edit linux download

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 #### Linux
 ```bash
-curl -L https://github.com/Bakminstof/pki-chain-builder/raw/refs/heads/release/scripts/linux/get-repo.sh | bash && rm get-repo.sh
+curl -sSL https://github.com/Bakminstof/pki-chain-builder/raw/refs/heads/release/scripts/linux/get-repo.sh | bash
 ```
--- scripts/linux/get-repo.sh
+++ scripts/linux/get-repo.sh
@@ -3,6 +3,8 @@ REPO_URL="https://github.com/Bakminstof/pki-chain-builder"
 REPO_NAME="pki-chain-builder"
 BRANCH="release"
 
+PYTHON="$(pwd)/$REPO_NAME/env/python-3.12.10-embed-amd64-linux/bin/python3.12"
+
 git clone --depth=1 --single-branch --branch "$BRANCH" --no-checkout "$REPO_URL" "$REPO_NAME"
 
 cd "$REPO_NAME"
@@ -12,3 +14,5 @@ git sparse-checkout set --no-cone "/.gitattributes" "/.gitignore" "!/env" "/env/
 git checkout "$BRANCH"
 git reset --hard "origin/$BRANCH"
 git pull -X theirs "origin" "$BRANCH"
+
+chmod +x "$PYTHON"
